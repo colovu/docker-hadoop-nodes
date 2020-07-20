0aedc4d908abc405adbd7ebb2f2e5f17319351d0
Endial Fang <endial@126.com>
2020-07-20 09:47:14 +0800

    [feat:3.2]增加HistoryServer镜像Dockerfile文件及脚本文件

---
+++ historyserver/run.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+. /usr/local/bin/appcommon.sh			# 应用专用函数库
+
+eval "$(docker_app_env)"
+
+# 需要启动两个？
+# sbin/mr-jobhistory-daemon.sh start historyserver 
+# sbin/yarn-daemon.sh start timelineserver
+
+yarn --config ${APP_CONF_DIR} historyserver
