9d0d816fd2191ec2e28da4cfc272fc06460f5f65
Endial Fang <endial@126.com>
2020-11-30 16:16:54 +0800

    [fix:3.3]更新namenode调试信息

--- namenode/customer/usr/local/bin/run.sh
+++ namenode/customer/usr/local/bin/run.sh
@@ -18,7 +18,9 @@ fi
 #   - 如果 当前节点 与 集群活跃节点 相同，则初始化数据，并激活 zkfc
 #   
 if [ "`ls -A ${HDFS_NAMENODE_DATA_DIR}`" == "" ]; then
-	if [ "${HADOOP_ACTIVED_NAMENODE}" = "${HADOOP_CURRENT_NAMENODE}" ]; then
+	LOG_D "Active Namenode: ${HADOOP_ACTIVED_NAMENODE}"
+	LOG_D "Current Namenode: ${HADOOP_CURRENT_NAMENODE}"
+	if [ "${HADOOP_ACTIVED_NAMENODE:-}" = "${HADOOP_CURRENT_NAMENODE}" ]; then
   		if [[ -n "${CORE_CONF_ha_zookeeper_quorum}" ]]; then
   			LOG_I "Formatting Zookeeper Failover Controller"
 			hdfs --config ${APP_CONF_DIR} zkfc -formatZK -nonInteractive
@@ -29,16 +31,22 @@ if [ "`ls -A ${HDFS_NAMENODE_DATA_DIR}`" == "" ]; then
   		LOG_I "Start Zookeeper Failover Controller for Namenode HA"
   		hdfs --config ${APP_CONF_DIR} --daemon start zkfc
   	else
-  		if [[ -n "${HADOOP_ACTIVED_NAMENODE}" ]]; then
+  		if [[ -n "${HADOOP_ACTIVED_NAMENODE:-}" ]]; then
   			LOG_I "Formatting secondary namenode, directory: ${HDFS_NAMENODE_DATA_DIR}"
-  			hdfs --config ${APP_CONF_DIR} namenode -bootstrapStandby -nonInteractive -skipSharedEditsCheck
+  			#hdfs --config ${APP_CONF_DIR} namenode -bootstrapStandby -nonInteractive -skipSharedEditsCheck
+  			hdfs --config ${APP_CONF_DIR} namenode -bootstrapStandby -nonInteractive
   			LOG_I "Start Zookeeper Failover Controller for Namenode HA"
   			hdfs --config ${APP_CONF_DIR} --daemon start zkfc
   		else
-  			LOG_I "Formatting namenode(HA) data directory: ${HDFS_NAMENODE_DATA_DIR}"
+  			LOG_I "Formatting namenode data directory: ${HDFS_NAMENODE_DATA_DIR}"
   			hdfs --config ${APP_CONF_DIR} namenode -format -nonInteractive
   		fi
   	fi
+else
+	if [[ -n "${HADOOP_ACTIVED_NAMENODE:-}" ]]; then
+		LOG_I "Start Zookeeper Failover Controller for Namenode HA"
+		hdfs --config ${APP_CONF_DIR} --daemon start zkfc
+	fi
 fi
 
 LOG_I "Start NameNode process..."
